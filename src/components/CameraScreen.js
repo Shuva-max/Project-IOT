3295c871bdd79543bad7f2541f72179e5b25a455
Shuva Paul <shuva575paul@gmail.com>
2023-09-20 20:25:24 +0530

    Basic layout design is going on

---
+++ src/components/CameraScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CameraScreen() {
+  return (
+    <div className='cameraScreen'>
+
+        <div className='c1'>
+
+        </div>
+      
+    </div>
+  )
+}
